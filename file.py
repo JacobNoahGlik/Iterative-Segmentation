18b5d0a672a68e4e3bae9b2fdd2fdfae2df1bf71
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-04-13 12:00:16 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,24 @@
 class Function:
-  def __init__(self):
-    pass
+  def __init__(self, name, args, body, prefix_text = ''):
+    self.name = name
+    self.args = args
+    self.body = body
+    self.prefix = prefix_text
 
 class Class:
-  def __init__(self):
-    pass
+  def __init__(self, name, body):
+    self.name = name
+    self.body = body
+    self.functions = []
+    self.indentation = ''
+    self.deps = []
+    self._scan_body()
 
 class File:
-  def __init__(self):
-    pass
+  def __init__(self, name):
+    self.name = name
+    self.body = ''
+    self.classes = []
+    self.functions = []
+    self.deps = []
+    self._scan_file()
