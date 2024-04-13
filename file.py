ec5200ede7cf515567e565400e3cbf3d3d5339a4
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-04-13 12:02:01 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,24 +1,24 @@
 class Function:
-  def __init__(self, name, args, body, prefix_text = ''):
-    self.name = name
-    self.args = args
-    self.body = body
-    self.prefix = prefix_text
+  def __init__(self, name: str, args: list[str], body: str, prefix_text: str = ''):
+    self.name: str = name
+    self.args: list[str] = args
+    self.body: str = body
+    self.prefix: str = prefix_text
 
 class Class:
-  def __init__(self, name, body):
-    self.name = name
-    self.body = body
-    self.functions = []
-    self.indentation = ''
-    self.deps = []
+  def __init__(self, name: str, body: str):
+    self.name: str = name
+    self.body: str = body
+    self.functions: list[Function] = []
+    self.indentation: str = ''
+    self.deps: list[str] = []
     self._scan_body()
 
 class File:
-  def __init__(self, name):
-    self.name = name
-    self.body = ''
-    self.classes = []
-    self.functions = []
-    self.deps = []
+  def __init__(self, name: str):
+    self.name: str = name
+    self.body: str = ''
+    self.classes: list[Class] = []
+    self.functions: list[Function] = []
+    self.deps: list[str] = []
     self._scan_file()
